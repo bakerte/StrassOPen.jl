fb43f26efb148984ac5f8169d535139ab1ebf867
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2025-01-01 01:52:31 -0800

    Add files via upload

---
+++ lib/imports.jl
@@ -0,0 +1,12 @@
+#########################################################################
+#
+#           Strassen Open Implementation (StrassOPen)
+#                          v1.0
+#
+#########################################################################
+# Made by Thomas E. Baker, Kiana Gallagher, and Aaron Dayton and « les qubits volants » (2024)
+# See accompanying license with this program
+# This code is native to the julia programming language (v1.10.4+)
+#
+
+
---
+++ lib/types.jl
@@ -0,0 +1,17 @@
+#########################################################################
+#
+#           Strassen Open Implementation (StrassOPen)
+#                          v1.0
+#
+#########################################################################
+# Made by Thomas E. Baker, Kiana Gallagher, and Aaron Dayton and « les qubits volants » (2024)
+# See accompanying license with this program
+# This code is native to the julia programming language (v1.10.4+)
+#
+
+"""
+    file_extension
+
+A default file extension can be specified for the large types
+"""
+const file_extension = ".strassOPen"
